c8e6607e050fa53555e40adf665875328cf88d11
Anthony Tsai <johnnytsai81@gmail.com>
2022-12-24 20:56:15 +0800

    調整導向

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -9,9 +9,9 @@ const HomePage = () => {
 
   useEffect(() => {
     if (isAuthenticated) {
-      navigate("/login");
-    } else {
       console.log(5)
+    } else {
+      navigate("/login");
     }
   }, [navigate, isAuthenticated]);
 
